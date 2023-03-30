294475de6a976e3780f5bc8eb56dea5fb2414479
Sumon Datta <sumondatta16@gmail.com>
2023-03-30 17:23:33 +0600

    responsive navbar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector("#menubar")
+
+const navbars = document.querySelector(".navbar")
+
+const handleMenubar = () =>{
+    menu.classList.toggle("fa-times")
+    navbars.classList.toggle("active")
+}
